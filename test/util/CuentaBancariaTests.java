c5eedfcedaba6df4357e7398d424c4c69f2fb2a1
Julián Ariel Martinez <julymartinez94@gmail.com>
2017-09-05 12:06:46 -0300

    ROJO: Se agrega caso para n = 20

--- test/util/CuentaBancariaTests.java
+++ test/util/CuentaBancariaTests.java
@@ -20,8 +20,8 @@ public class CuentaBancariaTests {
 	@Test
 	public void transfiereSaldoHacia() {
 		
-		cuentaA.transferirMontoHacia(10, cuentaB);
-		Assert.assertEquals(10, cuentaB.consultarSaldo(), 0);
+		cuentaA.transferirMontoHacia(20, cuentaB);
+		Assert.assertEquals(20, cuentaB.consultarSaldo(), 0);
 	}
 
 }
