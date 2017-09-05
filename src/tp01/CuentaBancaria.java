a70f6fea6bb75015f5d7570974d4c3cb4e2b8820
Julián Ariel Martinez <julymartinez94@gmail.com>
2017-09-05 12:16:04 -0300

    AZUL: Refactor de la suma de saldos

--- src/tp01/CuentaBancaria.java
+++ src/tp01/CuentaBancaria.java
@@ -15,6 +15,6 @@ public class CuentaBancaria {
 	}
 	
 	public void transferirMontoHacia(double n, CuentaBancaria cuentaDestino) {
-		cuentaDestino.n = cuentaDestino.n + n;
+		cuentaDestino.n += n;
 	}
 }
